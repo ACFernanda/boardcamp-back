bc7ad1d10a7e3979eeb80277c4098f413e2ee2a4
acfernanda <acfernanda98@gmail.com>
2022-05-28 07:43:21 -0300

    Feat getAllRentals and addRental

--- controllers/rentalsController.js
+++ controllers/rentalsController.js
@@ -1,12 +1,34 @@
+import dayjs from "dayjs";
+import joi from "joi";
+
 import db from "./../db.js";
 
 export async function getAllRentals(req, res) {
   try {
-    const result = await db.query(`SELECT * FROM rentals`);
+    const resultRentals = await db.query(`SELECT * FROM rentals`);
+    const rentals = resultRentals.rows;
 
-    // ADICIONAR CUSTOMER E GAME - JOIN ENCADEADO
+    let completeRentals = [];
+    let completeRental = null;
+    let customer = null;
+    let game = null;
+    for (let rental of rentals) {
+      const customerResult = await db.query(
+        `SELECT * FROM customers WHERE id = $1`,
+        [rental.customerId]
+      );
+      customer = customerResult.rows[0];
 
-    res.send(result.rows);
+      const gameResult = await db.query(`SELECT * FROM games WHERE id = $1`, [
+        rental.gameId,
+      ]);
+      game = gameResult.rows[0];
+
+      completeRental = { ...rental, customer, game };
+      completeRentals.push(completeRental);
+    }
+
+    res.send(completeRentals);
   } catch (e) {
     console.log(e);
     res.status(500).send("Ocorreu um erro ao obter as categorias!");
@@ -15,11 +37,43 @@ export async function getAllRentals(req, res) {
 
 export async function addRental(req, res) {
   const newRental = req.body;
+  const today = dayjs().format("YYYY-MM-DD");
+
   try {
+    const checkCostumer = await db.query(
+      `SELECT * FROM customers WHERE id = $1`,
+      [newRental.customerId]
+    );
+    const customer = checkCostumer.rows[0];
+
+    if (customer === undefined) {
+      res.sendStatus(400);
+      return;
+    }
+
+    const checkGame = await db.query("SELECT * FROM games WHERE id = $1", [
+      newRental.gameId,
+    ]);
+
+    const game = checkGame.rows[0];
+
+    if (game === undefined) {
+      res.sendStatus(400);
+      return;
+    }
+
+    const originalPrice = game.pricePerDay * newRental.daysRented;
+
     const result = await db.query(
-      `INSERT INTO rentals ("customerId", "gameId", "daysRented")
-       VALUES ($1, $2, $3)`,
-      [req.body.customerId, req.body.gameId, req.body.daysRented]
+      `INSERT INTO rentals ("customerId", "gameId", "rentDate", "daysRented", "returnDate", "originalPrice", "delayFee")
+       VALUES ($1, $2, $3, $4, null, $5, null)`,
+      [
+        req.body.customerId,
+        req.body.gameId,
+        today,
+        req.body.daysRented,
+        originalPrice,
+      ]
     );
     res.sendStatus(201);
   } catch (e) {
--- routes/rentalsRouter.js
+++ routes/rentalsRouter.js
@@ -1,11 +1,14 @@
 import { Router } from "express";
-// import { getAllRentals, addRental, endRental, deleteRental } from "./../controllers/rentalsController.js";
+import {
+  getAllRentals,
+  addRental,
+} from "./../controllers/rentalsController.js";
 
 const rentalsRouter = Router();
 
-// rentalsRouter.get("/rentals", getAllRentals);
+rentalsRouter.get("/rentals", getAllRentals);
 
-// rentalsRouter.post("/rentals", addRental);
+rentalsRouter.post("/rentals", addRental);
 
 // rentalsRouter.post("/rentals/:id/return", endRental);
 
